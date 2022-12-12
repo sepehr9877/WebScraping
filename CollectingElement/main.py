510024ddbfbabd0cf5de668176aa6ab2b233eec2
sepehr9877 <sepehr.seifpour@yahoo.com>
2022-12-11 19:33:57 -0500

    starting the project

--- CollectingElement/main.py
+++ CollectingElement/main.py
@@ -1,38 +1,7 @@
 from pprint import pprint
-import asyncio
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
-import webbrowser
-import requests
-from time import sleep
-async def send_request(url):
-    headers={ "User-Agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
-              }
-    request_url=requests.request(method="get",url=url)
-    return request_url
-async def getelements():
-    get_request=await send_request("https://www.coindesk.com/price/bitcoin/")
-    soup=BeautifulSoup(get_request.content,'lxml')
-    div_elements=soup.find_all('div',class_="Box-sc-1hpkeeg-0 eQeCqF")
-    price=[]
-    for element in div_elements:
-        element_name=soup.find('div',class_="Box-sc-1hpkeeg-0 iHOgZm")
-        print("elementName")
-        print(element_name.text)
-        element_price=element.findNext('div',class_="Box-sc-1hpkeeg-0 jwYVXk")
-        el=element_price.find('span',class_="currency-pricestyles__Price-sc-1rux8hj-0 jIzQOt")
-        print("elementPrice")
-        print(el.text)
-        whole_element_24h=element.findNext('div',class_="Box-sc-1hpkeeg-0 bDRvIB")
-        element_24h=whole_element_24h.findNext('div',class_="Box-sc-1hpkeeg-0 keDxjI")
-        print("24%")
-        print(element_24h.text)
-        whole_element_24Percent=element.find_all_next('div', class_="Box-sc-1hpkeeg-0 gRAZAN")
-        low24_element=whole_element_24Percent[0].findNext('div', class_="Box-sc-1hpkeeg-0 keDxjI")
-        print("low")
-        print(low24_element.text)
-        high24_element=whole_element_24Percent[1].findNext('div',class_="Box-sc-1hpkeeg-0 keDxjI")
-        print("high")
-        print(high24_element.text)
+from WebScrapingClass import Webscrape
+webscraping=Webscrape("ETH-USD")
+data=webscraping.json_data()
+pprint(data)
 
-asyncio.run(getelements())
+
--- Price/API/serializers.py
+++ Price/API/serializers.py
@@ -2,3 +2,25 @@ from rest_framework.serializers import Serializer,CharField
 
 class PriceSerializer(Serializer):
     Name=CharField()
+    WeekRange=CharField()
+    Circulating_supply=CharField()
+    Days_Range=CharField()
+    Maket_Cap=CharField()
+    Max_Supply=CharField()
+    Open=CharField()
+    Previous=CharField()
+    RegularMarketChange=CharField()
+    RegularMarketChangePercent=CharField()
+    StartDate=CharField()
+    Volume=CharField()
+    Volume_24H=CharField()
+    Volum_24H_All_Currencies=CharField()
+
+
+class SetUrlSerializer:
+    Url=CharField()
+
+    def create(self):
+        pass
+
+
