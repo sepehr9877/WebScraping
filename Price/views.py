e261ac85621eef97781b5ebb0c966fa34cdf46f0
sepehr9877 <sepehr.seifpour@yahoo.com>
2022-12-12 12:04:55 -0500

    changes in setting

--- Price/views.py
+++ Price/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseNotFound
+def error_404_view(request,exception):
+    return HttpResponseNotFound({"Not Found"})
 # Create your views here.
